2a66f1ba9f70dbecf097577572b26560ec419292
subhash m gowda <subhash m gowda@LAPTOP-TLMU690C>
2023-11-25 11:35:01 +0530

    day one test script

---
+++ src/demogithub/subhash.java
@@ -0,0 +1,5 @@
+package demogithub;
+
+public class subhash {
+
+}
---
+++ src/demogithub/subhash1.java
@@ -0,0 +1,10 @@
+package demogithub;
+
+public class subhash1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
